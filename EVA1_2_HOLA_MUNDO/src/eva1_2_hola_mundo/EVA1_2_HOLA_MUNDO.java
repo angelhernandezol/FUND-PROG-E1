7b32a16fb0000cefbc9aeb436247115304b5dc6c
angelhernandezol <L26550212@chihuahua2.tecnm.mx>
2026-09-03 18:17:05 -0600

    Add files via upload

---
+++ EVA1_2_HOLA_MUNDO/src/eva1_2_hola_mundo/EVA1_2_HOLA_MUNDO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_hola_mundo;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_2_HOLA_MUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //INICIO DEL PROGRAMA
+
+//ESTO ES UN BLOQUE DE CODIGO
+//MOSTRAR UN MENSAJDE TEXTO
+//MOSTRAR --> INSTRUCCION
+//TEXTO --> DATOS
+// TODO code application logic here
+
+System.out.println("Hola Mundo");
+    }
+    
+}
