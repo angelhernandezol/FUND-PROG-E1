a763dd039ac7dcd129d9b310795767b1f8572f9d
angelhernandezol <L26550212@chihuahua2.tecnm.mx>
2026-09-03 18:43:24 -0600

    Add files via upload

---
+++ EVA1_3_SALIDAS/src/eva1_3_salidas/EVA1_3_SALIDAS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_3_SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bienvenidos a Fundamentos de la progrmacion");
+        System.out.println("Angel Mauricio Hernandez Olivas");
+        System.out.print("Tu calificacion sera: ");
+        System.out.println("86");
+
+    }
+    
+}
