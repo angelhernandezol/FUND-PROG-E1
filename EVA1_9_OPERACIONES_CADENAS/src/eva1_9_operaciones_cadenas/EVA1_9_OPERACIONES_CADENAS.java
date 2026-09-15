fe623d4c1946751cf1c3d648735585f70d1be199
angelhernandezol <L26550212@chihuahua2.tecnm.mx>
2026-09-14 18:52:17 -0600

    Add files via upload

---
+++ EVA1_9_OPERACIONES_CADENAS/src/eva1_9_operaciones_cadenas/EVA1_9_OPERACIONES_CADENAS.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_operaciones_cadenas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_9_OPERACIONES_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //CONCATENACIÓN: PEGAR CADENAS DE TEXTO
+        //Cuando un operador hace diferentes acciones se le llama SOBRECARGA
+        
+        String cade;
+        
+        //No hay menos en cadenas, solo sumas
+        cade = "Hola" + " " + "mundo";
+        System.out.println(cade);
+        
+        cade = "Valor = " + 100;
+        System.out.println(cade);
+        
+        cade = "Valor booleanos = " + false;
+        System.out.println(cade);
+        
+        
+    }
+    
+    
+}
