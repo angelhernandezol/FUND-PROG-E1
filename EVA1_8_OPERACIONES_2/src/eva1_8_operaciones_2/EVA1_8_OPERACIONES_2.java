13cb8d7946373a5cd5f74b65d02f0f2e110059a7
angelhernandezol <L26550212@chihuahua2.tecnm.mx>
2026-09-14 18:48:27 -0600

    Add files via upload

---
+++ EVA1_8_OPERACIONES_2/src/eva1_8_operaciones_2/EVA1_8_OPERACIONES_2.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_operaciones_2;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_8_OPERACIONES_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int resu, val1 = 10, val2 = 5; //declara e INICIALIZANDO
+        
+        //JAVA INICIALIZA EN AUTOMÁTICO LAS VARIABLES
+        //NÚMERO EN CERO, BOOLEANOS EN FALSO
+        
+        resu = 100 + val1 * (val1 / val2);
+        
+        System.out.print("Resu = ");
+        System.out.println(resu);
+        
+        
+        
+        
+    }
+    
+}
